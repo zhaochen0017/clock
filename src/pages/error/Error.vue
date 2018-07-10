dde9ba83ae38260c4e2a2148ebd5dfc5a923894b
zhao chen <zhaochen0017@qq.com>
2018-07-10 18:17:51 +0800

    上传我的项目

---
+++ src/pages/error/Error.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="header">
+      鉴权失败
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Error'
+}
+</script>
+
+<style lang="stylus" scoped>
+ .header
+    width : 100%
+    background : #2680c4
+    color : #fff
+    text-align : center
+    font-size 16px
+    line-height : 2.8
+</style>
